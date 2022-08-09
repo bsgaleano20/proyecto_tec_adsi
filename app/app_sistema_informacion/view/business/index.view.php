a0fa2de2051a406de35f92e85cd7361840ad40a5
bsgaleano20 <bsgaleano20@misena.edu.co>
2022-08-08 20:16:24 -0500

    Cambio de colores de la pagina de prueba

--- app/app_sistema_informacion/view/business/index.view.php
+++ app/app_sistema_informacion/view/business/index.view.php
@@ -5,8 +5,70 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Empresa</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
-    <h1>Estoy en la vista de la Empresa (Landing)</h1>
+   <div class="contenedor">
+    <!-- navegador -->
+    <div class="nav">
+       <div class="borde logo"></div>
+       <div class="borde menu"></div>
+       <div class="borde ingreso">INGRESO</div>
+    </div>
+    <br>
+    <!-- encabezado -->
+    <div class="cuerpo">
+       <div class=" borde encabezado"></div>
+    </div>
+    <!-- titulo portafolio -->
+    <br>
+    <div class="tportafolio">
+       <div class="borde titulo_portafolio">BUENOS DIAS</div>
+    </div>
+    <br>
+    <!-- seccion portafolio -->
+    <div class="seccion">
+       <div class="borde cuadro1">SUBTITULO</div>
+       <div class="borde cuadro2">SUBTITULO</div>
+       <div class="borde cuadro3">SUBTITULO</div>
+    </div>
+    <!-- quienes somos -->
+    <br>
+    <div class="qsomos">
+       <div class="borde quienes_somos">QUIENES SOMOS</div>
+    </div>
+    <!-- seccion nosotros -->
+    <div class="seccionnosotros">
+       <div class="borde nosotros1">SUBTITULO</div>
+       <div class="borde nosotros2 nosotros_mid"></div>
+       <div class="borde nosotros3"></div>
+    </div>
+    <div class="seccionnosotros">
+       <div class="borde nosotros1 nosotros_mid"></div>
+       <div class="borde nosotros2"></div>
+       <div class="borde nosotros3 nosotros_mid">SUBTITULO</div>
+    </div>
+    <div class="seccionnosotros">
+       <div class="borde nosotros1">SUBTITULO</div>
+       <div class="borde nosotros2 nosotros_mid"></div>
+       <div class="borde nosotros3"></div>
+    </div>
+    <!-- titulo contactenos -->
+    <br>
+    <div class="contactenos">
+       <div class="borde contactenos1">CONTACTENOS</div>
+    </div>
+    <br>
+    <!-- seccion contactenos -->
+    <div class="sconta">
+       <div class="borde scontac1"></div>
+       <div class="borde scontac2">CONTACTA CON NOSOTROS</div>
+       <div class="borde scontac3"></div>
+    <!-- pie contactenos -->
+    <br>
+    <div class="pcontactenos">
+       <div class="borde pcontactenos1">inicio  portafolio</div>
+    </div>
+    </div>
 </body>
 </html>
