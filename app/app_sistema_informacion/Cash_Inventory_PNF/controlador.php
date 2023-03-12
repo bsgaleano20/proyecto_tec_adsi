fd2928580e6e5085996d09f3a07897ed44ba1f18
bsgaleano20 <94256618+bsgaleano20@users.noreply.github.com>
2023-03-11 21:43:45 -0500

    Update Controller login
    Quemado de codigo del login

--- app/app_sistema_informacion/Cash_Inventory_PNF/controlador.php
+++ app/app_sistema_informacion/Cash_Inventory_PNF/controlador.php
@@ -6,19 +6,8 @@ $estado_sesion = false;
 
 session_start();
 
-$_SESSION['email'] = $usuario;
-
-include('conexion.php');
-
-$consulta = "SELECT * FROM usuario WHERE email = '$usuario' and password = '$password'";
-$resultado = mysqli_query($conexion, $consulta);
-
-
-$filas = mysqli_num_rows($resultado);
-
-if ($filas){
+if ($usuario == "admin@cashinventory.com" and $password == "Admin.123456"){
     header("location:modules/administrador/home.php");
-    $estado_sesion = true;
 }
 else{
     ?>
@@ -31,6 +20,32 @@ else{
     <h6 class="alert alert-danger text-center">DATOS INCORRECTOS</h6>
     <?php
 }
+
+// $_SESSION['email'] = $usuario;
+
+// include('conexion.php');
+
+// $consulta = "SELECT * FROM usuario WHERE email = '$usuario' and password = '$password'";
+// $resultado = mysqli_query($conexion, $consulta);
+
+
+// $filas = mysqli_num_rows($resultado);
+
+// if ($filas){
+//     header("location:modules/administrador/home.php");
+//     $estado_sesion = true;
+// }
+// else{
+//     ?>
+//     <?php
+//     include("login.php");
+//     ?>
+//     <!-- <div class="alert alert-danger" role="alert">
+//         A simple danger alert—check it out!
+//     </div> -->
+//     <h6 class="alert alert-danger text-center">DATOS INCORRECTOS</h6>
+//     <?php
+// }
 
 mysqli_free_result($resultado);
 mysqli_close($conexion);
